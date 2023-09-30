dc31849160b235d2db5c695e811383824b9d5976
Nadin <esperansa10@gmail.com>
2023-09-30 13:44:26 +0300

    added brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -12,4 +12,5 @@ function greet()
     $name = prompt('May I have your name?');
 
     line("Hello, %s!", $name);
+     
 }
---
+++ src/Even.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BrainGames\Even;
+
+use BrainGames\Cli;
+use function cli\greet;
+use function cli\line;
+use function cli\prompt;
+
+
+function even()
+{
+   line('Answer "yes" if the number is even, otherwise answer "no".');
+   $random = '';
+    
+    for($i = 1; $i <= 3; $i++) {
+
+        line($random = rand(1, 10)); 
+        $answer = prompt('Your answer');
+        
+        if ($random % 2 === 1 && $answer === 'yes') {
+            return line("'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again," . $name . "!"); ; 
+        }
+        else if ($random % 2 === 0 & $answer === 'no')  {
+            return line("'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again," . $name . "!"); 
+        }
+        else if ($random % 2 === 1 & $answer !== 'no')  {
+            return line("'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again," . $name . " "); 
+        }
+        else if ($random % 2 === 0 & $answer !== 'yes')  {
+            return line("Sorry, I didn't understand you, please repeat"); 
+        }
+        else {
+            line("Correct");
+        }
+        
+    }
+    line("Congratulations, " . $name . "!");
+} 
+
+ 
