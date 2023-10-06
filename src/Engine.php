ecf01024cf4511fe28346b3e03251f2b509d2fac
Nadin <esperansa10@gmail.com>
2023-10-06 12:08:14 +0300

    second version

--- src/Engine.php
+++ src/Engine.php
@@ -2,15 +2,55 @@
 
 namespace BrainGames\Engine;
 
+use BrainGames\Even;
+use BrainGames\Calc;
 use function cli\line;
 use function cli\prompt;
+use function Even\even;
+use function Calc\calc;
 
-function engine($answer, $result, $username) {
+// function engine($answer, $result, $username, $type)
+function engine($type) {
 
-    if($answer === $result) {
-    return line("Correct");
+line('Welcome to the Brain Game!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
+
+    if ($type === 'even') {
+        line('Answer "yes" if the number is even, otherwise answer "no".');
+        
     }
     else {
-    return line($answer . " is wrong answer ;(. Correct answer was " . $result .". Let's try again, " . $username . "!"); 
+        line('What is the result of the expression?');
+    }
+
+    if ($type === 'even') {
+     
+        Even\even($name); 
+    } 
+    else {
+        Calc\calc($name); 
+        }; 
+
+        return line("Congratulations, " .  $name . "!");
+
+    }       
+
+
+
+
+function results($answer, $result, $username) {
+    
+
+    if($answer === $result) {
+        for($i = 0; $i <= 4; $i++) { 
+            $i++; 
+        line("Correct");
+        Even\even($username);  
+        }
+    }
+        else {
+        return  line($answer . " is wrong answer ;(. Correct answer was " . $result .". Let's try again, " . $username . "!"); 
+    }
 }
-}
+
--- src/Games/Even.php
+++ src/Games/Even.php
@@ -12,52 +12,46 @@ use function Engine\engine;
  
 function even($name)
 {
-   line('Answer "yes" if the number is even, otherwise answer "no".');
+   
    $random = '';
-   $result = ''; 
-   $username = $name; 
-    for($i = 1; $i <= 3; $i++) {
-
-        line($random = rand(1, 10)); 
+   $username = $name;     
+    line($random = rand(1, 10)); 
         $answer = prompt('Your answer');
         
         if ($random % 2 === 1 && $answer === 'yes') {
-            $result = 'no'; 
-            $answer = 'yes'; 
-            return Engine\engine($answer, $result,  $username);             
+          $result = 'no';   $answer = 'yes'; 
+                       
         }
         else if ($random % 2 === 0 & $answer === 'no')  {
-            $result = 'yes'; 
-            $answer = 'no'; 
-            return  Engine\engine($answer, $result,  $username);             
+          $result = 'yes'; 
+           $answer = 'no'; 
+           return  Engine\results($answer, $result, $username);             
            
         }
         else if ($random % 2 === 1 & $answer !== 'no')  {
-            $result = 'yes'; 
-            $answer = 'no'; 
-            return  Engine\engine($answer, $result,  $username);
+           $result = 'yes'; 
+           $answer = 'no'; 
+           return  Engine\results($answer, $result, $username); 
             
         }
         else if ($random % 2 === 0 & $answer !== 'yes')  {
-            $result = 'no';
+         $result = 'no';
             $answer = 'yes'; 
-            return  Engine\engine($answer, $result,  $username);
+            return  Engine\results($answer, $result, $username); 
            
         }
         else if  ($random % 2 === 0 & $answer === 'yes'){
             $result = 'yes';
             $answer = 'yes'; 
-            Engine\engine($answer, $result,  $username);
+            return  Engine\results($answer, $result, $username); 
         }
         else if ($random % 2 === 1 & $answer === 'no'){
-            $result = 'no';
-            $answer = 'no'; 
-            Engine\engine($answer, $result, $username);
-        }
-
-           
+           $result = 'no';
+           $answer = 'no'; 
+           return  Engine\results($answer, $result, $username); 
+        }    
     }
-    return line("Congratulations, " .  $username . "!");
-} 
+    
+
 
  
